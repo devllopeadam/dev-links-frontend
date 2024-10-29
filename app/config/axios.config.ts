42146238e5fb4f199fb892647c69df72aa10c9ed
devllopeadam <jeniahadam@gmail.com>
2024-10-29 21:45:19 +0100

    modifying some things

--- app/config/axios.config.ts
+++ app/config/axios.config.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:1337/api",
+  baseURL:
+    `${process.env.NEXT_PUBLIC_API_URL}/api` || "http://localhost:1337/api",
 });
 
 export default axiosInstance;
